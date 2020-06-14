9be10263e6ed3b0de7c711d953b3bfb00c36a2d1
BarnyK <barny@DESKTOP-E6QQNDG.localdomain>
2020-06-14 09:16:56 +0200

    renaming files

--- second.s
+++ second.s
@@ -11,6 +11,7 @@ draw_ean8:
     mov     [eax+63], DWORD 0x01000100
     mov     [eax+31], DWORD 0x010001
     mov     [eax+33], DWORD 0x010001
+    
     mov     esp, ebp
     pop     ebp
     ret 
