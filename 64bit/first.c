b1a7e70ffd7d617f39152fd33c2cdfdb98381f32
BarnyK <barny@DESKTOP-E6QQNDG.localdomain>
2020-06-16 18:30:08 +0200

    64 bit without floats

--- 64bit/first.c
+++ 64bit/first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdint.h>
 //void draw_ean8(void *img, unsigned int stride, unsigned int height,unsigned int modwidth, char *digits);
 extern void draw_ean8(char *img, unsigned int stride, unsigned int height, unsigned int width, char *digits, char *buffer);
 
 // Writes header data and 2 color pallette
-void writeHeader(FILE *fptr, unsigned int file_size, unsigned int height, unsigned int width){
+void writeHeader(FILE *fptr, __uint32_t file_size, __int32_t height, __int32_t width){
     unsigned short type = 0x4d42;
     unsigned short reserved1 = 0;
     unsigned short reserved2 = 0;
@@ -22,30 +22,30 @@ void writeHeader(FILE *fptr, unsigned int file_size, unsigned int height, unsign
     unsigned int imporant_colors = 0;
     unsigned int color1 = 0x00FFFFFF;
     unsigned int color2 = 0x00000000;
-    fwrite(&type, sizeof(__uint16_t), 1, fptr);
-    fwrite(&file_size, sizeof(__uint32_t), 1, fptr);
-    fwrite(&reserved1, sizeof(__uint16_t), 1, fptr);
-    fwrite(&reserved2, sizeof(__uint16_t), 1, fptr);
-    fwrite(&start, sizeof(__uint32_t), 1, fptr);
-    fwrite(&header_size, sizeof(__uint32_t), 1, fptr);
-    fwrite(&width, sizeof(__int32_t), 1, fptr);
-    fwrite(&height, sizeof(__int32_t), 1, fptr);
-    fwrite(&planes, sizeof(__uint16_t), 1, fptr);
-    fwrite(&bits_per_pixel, sizeof(__uint16_t), 1, fptr);
-    fwrite(&compression, sizeof(__uint32_t), 1, fptr);
-    fwrite(&image_size, sizeof(__uint32_t), 1, fptr);
-    fwrite(&vertical_resolution, sizeof(__int32_t), 1, fptr);
-    fwrite(&horizontal_resolution, sizeof(__int32_t), 1, fptr);
-    fwrite(&colors, sizeof(__uint32_t), 1, fptr);
-    fwrite(&imporant_colors, sizeof(__uint32_t), 1, fptr);
-    fwrite(&color1, sizeof(__uint32_t), 1, fptr);
-    fwrite(&color2, sizeof(__uint32_t), 1, fptr);
+    fwrite(&type, sizeof(uint16_t), 1, fptr);
+    fwrite(&file_size, sizeof(uint32_t), 1, fptr);
+    fwrite(&reserved1, sizeof(uint16_t), 1, fptr);
+    fwrite(&reserved2, sizeof(uint16_t), 1, fptr);
+    fwrite(&start, sizeof(uint32_t), 1, fptr);
+    fwrite(&header_size, sizeof(uint32_t), 1, fptr);
+    fwrite(&width, sizeof(int32_t), 1, fptr);
+    fwrite(&height, sizeof(int32_t), 1, fptr);
+    fwrite(&planes, sizeof(uint16_t), 1, fptr);
+    fwrite(&bits_per_pixel, sizeof(uint16_t), 1, fptr);
+    fwrite(&compression, sizeof(uint32_t), 1, fptr);
+    fwrite(&image_size, sizeof(uint32_t), 1, fptr);
+    fwrite(&vertical_resolution, sizeof(int32_t), 1, fptr);
+    fwrite(&horizontal_resolution, sizeof(int32_t), 1, fptr);
+    fwrite(&colors, sizeof(uint32_t), 1, fptr);
+    fwrite(&imporant_colors, sizeof(uint32_t), 1, fptr);
+    fwrite(&color1, sizeof(uint32_t), 1, fptr);
+    fwrite(&color2, sizeof(uint32_t), 1, fptr);
 }
 
 // Image data stored in unsigner char
 
 int main(int argc, char *argv[]){
-    unsigned int width, height, checksum=0, codecheck = 1, digits_len, stride, file_size;
+    unsigned int width, height, checksum=0, codecheck = 1, digits_len, stride, file_size, modwith;
     char digits[9] = {0}, filename[256] = {0}, tmp;
     char *pictureData, *buffer;
     
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]){
 
     // Read and validate width
     width = atoi(argv[1]);
-    if(width < 67){
+    if(width < 67 || width %67 != 0){
         printf("Invalid width");
         return 2;
     }
@@ -116,10 +116,9 @@ int main(int argc, char *argv[]){
         return 5;
     }
     
-
-    draw_ean8(pictureData, stride, height, width, digits, buffer);
-
-
+    modwith = width / 67;
+    draw_ean8(pictureData, stride, height, modwith, digits, buffer);
+    
     FILE *fptr;
     fptr = fopen(filename,"wb");
     writeHeader(fptr, file_size, height, width);
--- 64bit/second.s
+++ 64bit/second.s
@@ -1,75 +1,80 @@
 section     .data
 codes:  db  0xD, 0x19, 0x13, 0x3D, 0x23, 0x31, 0x2F, 0x3B, 0x37, 0xB
-a:      dd  67.00
 
 section     .text
 global      draw_ean8
 
 draw_ean8:
+
+    ; Encoding digits to buffer
+    ; R9  - buffer pointer
+    ; R10 - digit counter 0 to 8
+    ; R11 - reading digit code
+    ; RAX - bit counter for code
+
     mov     [R9], DWORD 0xFF00FF           ;First brace
     mov     [R9+63], DWORD 0xFF00FF00      ;End brace
     mov     [R9+31], DWORD 0xFF00FF00      ;Second brace
     add     R9, 2                          ;allign for first digit
     
-    xor     R10, R10            ; zero edx for digit counter
-    xor     R11, R11            ; zero ebx for code reading
+    xor     R10D, R10D            ; zero edx for digit counter
+    xor     R11D, R11D            ; zero ebx for code reading
 read_dig:
     mov     R11B, [R8 + R10]          ; read digit
     sub     R11B, '0'                 ; digit to int
     mov     R11B, [codes + R11]       ; digit to code
 
-    mov     EAX, 7              ; init counter
-    cmp     R10, 4              ; for digits after 3 negate codes
+    mov     AX, 7              ; init counter
+    cmp     R10D, 4              ; for digits after 3 negate codes
     jl      loop1
     not     R11B
 loop1:
-    inc     R9                  ; inc buffer
-    dec     EAX                 ; decrease counter
-    bt      R11, RAX
+    inc     R9D                  ; inc buffer
+    dec     AX                 ; decrease counter
+    bt      R11B, AX
     jnc     loop1end            ; if 0 skip
     mov     [R9], BYTE 0xFF    
 loop1end:
     jnz     loop1               
-    inc     R10
+    inc     R10D
     cmp     R10D, 4
 
     jne     cont
-    add     R9, 5              ; skip the bar
+    add     R9D, 5              ; skip the bar
 cont:
-    cmp     R10, 8
+    cmp     R10D, 8
     jne     read_dig
 
-    sub     r9, 63              ; go back to beginning of buffer
-
-    ; Setting up scaling
-    CVTSI2SS    xmm1, RCX       ; xmm1 = width
-    movss   xmm0, [a]           ; xmm0 = 67
-    divss   xmm0, xmm1          ; xmm0 = 67/width = step
-    subss   xmm2, xmm2          
-
-    ; Preparation for loops
-    xor     R10, R10            ; 0 for width counter
-    xor     R11, R11            ; 0 for height counter
-    xor     RAX, RAX            ; 0 for buffer offset
+    ; Writing to picture data
+    ; R9  - buffer pointer
+    ; RDI - picture pointer
+    ; R10 - buffer offset/counter
+    ; R8  - saving and restoring picture pointer
+    ; R11 - height counter
+    ; RDX - height
+    ; RAX - modwith
+    ; RSI - stride
+    sub     R9D, 63                ; go back to beginning of buffer
+    xor     R10B, R10B            ; 0 for buffer offset/counter
 columnloop:
-    cmp     BYTE [R9 + RAX], 0
-    jz      afterset                ; Skip if 0
-
-    mov     R8, RDI             ; Save bottom of the column 
-    mov     R11, RDX            
-rowloop:
-    mov     [RDI + R10], BYTE 0xFF  
-    add     RDI, RSI                
+    cmp     [R9+R10], BYTE 0    
+    je      end
+    mov     R8, RDI                 ; save picture pointer
+    mov     R11, RDX
+loopheight:
+    mov     RAX, RCX       ; modwith
+loopmod:
+    mov     [RDI+RAX-1], BYTE 0xFF  
+    dec     EAX
+    jnz     loopmod                     
+    add     RDI, RSI              ; next row pointer
     dec     R11
-    jnz     rowloop
-
-    mov     RDI, R8             ; Restore column
-afterset:
-    addss   xmm2, xmm0            ; add step
-    cvttss2si   RAX, xmm2        ; round to integer with trunctuation
-    inc     R10
-    cmp     R10, RCX              ; RCX == height  
+    jnz     loopheight            ; jump next row if not done
+    mov     RDI, R8                    ; restore picture pointer
+end:
+    add     RDI, RCX               ; next section of barcode
+    inc     R10B         
+    cmp     R10B, 67
     jne     columnloop
 
-;Epilogue
     ret 
