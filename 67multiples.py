ee61149a54076b148157e263a94bd6e7634941af
Barny <barnaba.krupowicz@gmail.com>
2021-09-24 20:08:02 +0200

    comment

--- 67multiples.py
+++ 67multiples.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
 print('\n'.join(['\t'.join([str(67*(i+k)) for i in range(1,4)]) for k in range(0,40,3)]))
+# script to print out multiples of 67 that are accepted by the main draw_ean8 program
